19da9fbec6ce50c68366661299791fb3c07048ab
zydric.cit <zydric.abel@cit.edu>
2025-06-22 17:01:06 +0800

    Solve 05_joinStrings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,9 +8,15 @@
 */
 
 // Add your code right below, good luck!
+const firstName = 'Carlos'
+const lastName = 'Stevenson'
+const thisYear = 1965
+const birthYear = 1947
 
+const fullName = `${firstName} ${lastName}`
+const age = thisYear - birthYear
 
-
+let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 
 // Do not change this
