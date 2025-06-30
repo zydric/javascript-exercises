b49b04821247602d502a838aa72aaf403591a3ea
zydric.cit <zydric.abel@cit.edu>
2025-06-30 14:34:31 +0800

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+	let result = '';
+	if (num < 0) return 'ERROR';
 
+	for (let i = 0; i < num; i++) {
+		result += string;
+	}
+
+	return result;
 };
 
 // Do not edit below this line
