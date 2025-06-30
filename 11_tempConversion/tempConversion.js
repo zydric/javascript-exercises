415f64801f4dd6329b3122a4712b0dce86ab4522
zydric.cit <zydric.abel@cit.edu>
2025-06-30 16:33:57 +0800

    Solve 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	return Math.round(((fahrenheit - 32) / 1.8) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+	return Math.round(((celsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
