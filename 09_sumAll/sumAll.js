f124880de4fa89e3618a0663edcb87aee7723290
zydric.cit <zydric.abel@cit.edu>
2025-06-30 15:55:07 +0800

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+	if (num1 < 0 || num2 < 0) return 'ERROR';
 
+	const array = [];
+
+	let start = num1 < num2 ? num1 : num2;
+	const end = num1 > num2 ? num1 : num2;
+
+	for (let i = start; i <= end; i++) {
+		array.push(start++);
+	}
+
+	return array.reduce((sum, item) => (sum += item), 0);
 };
 
 // Do not edit below this line
