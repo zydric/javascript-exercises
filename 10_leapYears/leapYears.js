059d3454d5174433215ad22deea2e30d2f2e9d91
zydric.cit <zydric.abel@cit.edu>
2025-06-30 16:21:23 +0800

    Solve 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if(year % 4 == 0) {
+        if((year % 100 == 0) && !(year % 400 == 0)) {
+            isLeapYear = false;
+        } else {
+            isLeapYear = true;
+        }
+    }
 
+    return isLeapYear;
 };
 
 // Do not edit below this line
