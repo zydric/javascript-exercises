98d62fe6b12472b0f4c556039a46f067a427b60d
zydric.cit <zydric.abel@cit.edu>
2025-07-22 17:08:12 +0800

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	const age = (person) => {
+		const death = person.yearOfDeath || new Date().getFullYear();
+		return death - person.yearOfBirth;
+	};
 
+	return people.sort((a, b) => age(b) - age(a))[0];
 };
 
 // Do not edit below this line
