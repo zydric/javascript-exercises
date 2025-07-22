97a435c5d6b36c03e71b7f6624d6f1535fca9f69
zydric.cit <zydric.abel@cit.edu>
2025-07-22 13:06:53 +0800

    Solve 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, current) => acc += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for(let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
