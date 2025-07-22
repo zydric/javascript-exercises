a9705dc2317dd2113c28e995bcdb34584a0f2854
zydric.cit <zydric.abel@cit.edu>
2025-07-22 13:57:48 +0800

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    const filtered = words.toLowerCase().replace(/[^a-z0-9]/g, ''); 
+    const reversed = filtered.split('').reverse().join('');
+    return filtered === reversed;
 };
 
 // Do not edit below this line
