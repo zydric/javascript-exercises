8361a23915503c1e02d306fe601a2860819c6671
zydric.cit <zydric.abel@cit.edu>
2025-07-22 16:03:05 +0800

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = parseInt(n);
 
+	if (n === 0) {
+		return 0;
+	}
+
+	if (n < 1) {
+		return 'OOPS';
+	}
+
+	if (n === 1) {
+		return 1;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
